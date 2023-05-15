1d024ace9d374c7db07b41272f46c99ba5da21c3
MattEnjoys <matthieu.ebzant@orange.fr>
2023-05-15 15:05:22 +0200

    Commit 6 sous sixième branch, après avoir fini le projet en symfony, place aux bonus d'administration.

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Livre;
+use phpDocumentor\Reflection\Types\Boolean;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -15,7 +20,16 @@ class LivreCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield from parent::configureFields($pageName);
+        //yield from parent::configureFields($pageName);
+
+        yield TextField::new('titre');
+        yield TextField::new('isbn');
+        yield TextareaField::new('description');
+
+        yield BooleanField::new('archive');
+
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class);
+
         yield AssociationField::new('auteur');
         yield AssociationField::new('editeur');
         yield AssociationField::new('genres');
